20860506d7753bd09a66c6c0733f54520cf44269
Your Name <you@example.com>
2021-07-26 09:04:40 +0400

    Добавлена страница со свсеми задачами

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,38 @@
 <template>
-  <div class="">start</div>
+  <div class="container mx-auto">
+    <h1 class="text-center border-b-2 py-4">Список задач</h1>
+    <users-block
+      :usersTask='users'
+      :users='countUsers'
+    ></users-block>
+  </div>
 </template>
 
 <script>
-export default {}
+import usersBlock from '@/components/blocks/users'
+
+export default {
+  components: {
+    usersBlock
+  },
+  async fetch ({ store }) {
+    await store.dispatch('users/fetchTodos')
+  },
+  computed: {
+    users () {
+      return this.$store.getters['users/usersTodos']
+    },
+    countUsers () {
+      let countUsers = 0
+
+      this.users.forEach((user) => {
+        if (countUsers < parseFloat(user.userId)) {
+          countUsers = parseFloat(user.userId)
+        }
+      })
+
+      return countUsers
+    }
+  }
+}
 </script>
---
+++ pages/todos/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto text-center">
+    <h1>Список задач пользователя</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
